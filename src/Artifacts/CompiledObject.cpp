81be7b19aa70dab34390ab2f31927630c2681cc8
Timothy Hutchins <tgh6407@gmail.com>
2026-08-22 18:46:40 -0500

    Updated Artifacts system and misc
    I found it to be redundant to have Artifacts and Properties. An Artifact
    is really just a Property that got produced by a build. So now the
    Artifact system is tied into the Property system.

--- src/Artifacts/CompiledObject.cpp
+++
@@ -1,7 +0,0 @@
-module Lattice.Artifact.CompiledObject;
-
-using namespace Lattice;
-
-auto CompiledObject::GetFilePath() const -> std::filesystem::path {
-    return m_filePath;
-}
--- src/Object/Capabilities/ArtifactProvider.cpp
+++
@@ -1,7 +0,0 @@
-module Lattice.Object.Capabilities.ArtifactProvider;
-
-using namespace Lattice::Object::Capabilities;
-
-auto ArtifactProvider::AddArtifact(const Artifact &artifact) -> void {
-    m_artifacts.push_back(artifact);
-}
